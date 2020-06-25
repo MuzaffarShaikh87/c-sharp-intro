4f74261ef739dd9408afb9e18cee76e78631eba4
Muzaffar <muzaffarshaikh517@gmail.com>
2020-06-25 10:34:23 -0600

    Added datatype & Added mathematical operators.

--- Program.cs
+++ Program.cs
@@ -5,15 +5,37 @@ namespace c_sharp_intro
     class Program
     {
         static void Main(string[] args)
-        {
-            //User input
-            Console.WriteLine("Please Enter your Name: ");
-            //Assign variable to user name
-            string myName = Console.ReadLine();
-            //Display will show the collected name
-            Console.WriteLine("You have a great day " + myName);
-            //Used string interpolation (placeholder) to put values in string
-            Console.WriteLine("Hey {0}, You're looking great today!!!",myName);
+        {  
+            string myString = "This is a string!";
+            // Integer data type is used for whole number (no decimal..)
+            int myInt = 42;
+            // Double data type is used for decimal point number
+            double myDouble = 33.666;
+            //
+            bool myBool = true;
+
+            Console.WriteLine(myInt - 10); //32
+
+            Console.WriteLine(myInt * 2); //84 
+
+            Console.WriteLine(myInt / 2); //21
+
+            Console.WriteLine(myInt % 13);// 3
+
+            Console.WriteLine(myInt + 8); //50
+
+            Console.WriteLine("3" + "3"); //33
+
+            Console.WriteLine(3 + "3"); //33
+
+            Console.WriteLine("3" + 3); //33
+
+            Console.WriteLine(3 + 6 + "3"); //93
+
+
+
+
+
         }
     }
 }
