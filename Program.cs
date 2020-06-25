9b90b8e4f5488767372ae0b48c3f8a1ba3b19e89
Muzaffar <muzaffarshaikh517@gmail.com>
2020-06-25 00:25:00 -0600

    Add concatenation, add interpolation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //User input
+            Console.WriteLine("Please Enter your Name: ");
+            //Assign variable to user name
+            string myName = Console.ReadLine();
+            //Display will show the collected name
+            Console.WriteLine("You have a great day " + myName);
+            //Used string interpolation (placeholder) to put values in string
+            Console.WriteLine("Hey {0}, You're looking great today!!!",myName);
         }
     }
 }
