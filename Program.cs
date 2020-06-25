2dcef0c80994f506664cc24f9613f37a270c7ae9
Muzaffar <muzaffarshaikh517@gmail.com>
2020-06-25 13:19:08 -0600

    Add an if staement; Add an else statement

--- Program.cs
+++ Program.cs
@@ -6,38 +6,17 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {  
-            Console.WriteLine( true || true);
-            Console.WriteLine( false || true);
-            Console.WriteLine( false || false);
-            
-            Console.WriteLine( true && true);
-            Console.WriteLine( false && true);
-            Console.WriteLine( false && false);
+            Console.WriteLine("Please enter the string: ");
+            string userInput = Console.ReadLine();
 
-            Console.WriteLine( false == false);
-            Console.WriteLine( true == false);
-            Console.WriteLine( 2.25 == 2.25 );
-            Console.WriteLine( 2.25 == 3.14);
-
-            Console.WriteLine( 6 > 7 );
-            Console.WriteLine( 10 > 5 );
-
-            Console.WriteLine( 6 < 7 );
-            Console.WriteLine( 10 < 5 );
-
-            Console.WriteLine( 5 >= 5 );
-            Console.WriteLine( 4 >= 5 );
-
-            
-            Console.WriteLine( 7 <= 7 );
-            Console.WriteLine( 10 <= 5 );
-
-
-            Console.WriteLine( false != false);
-            Console.WriteLine( true != false);
-            Console.WriteLine( 5 != 5);
-            Console.WriteLine( 10 != 5);
-
+            if ( userInput.Length > 0)
+            {
+                Console.WriteLine("The user entered a value");
+            }
+            else
+            {
+                Console.WriteLine("The user did not entered a value");
+            }
         }
     }
 }
